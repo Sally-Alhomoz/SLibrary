60a527d22b1f544e0b981ae73cec9a2035209d94
smara <smara@LAPTOP-1E2M6GME>
2025-09-21 00:44:41 +0300

    Added a Reservation Class

--- Shared/Book.cs
+++ Shared/Book.cs
@@ -9,6 +9,7 @@ namespace Shared
     public class Book
     {
         public string Title { get; set; }
+        public string Author { get; set; }
         public int ID { get; set; }
         public int bookCount { get; set; } = 0;
         public int ReservedCount { get; set; }
@@ -19,18 +20,47 @@ namespace Shared
         {
             Title = "None";
             ID = -1;
+            Author = "None";
             bookCount = 1;
             AvailableCount = 1;
             ReservedCount = 0;
         }
 
-        public Book(string title, int id, int availableCount, int reservedCount)
+        public Book(int id , string title, string author,int availableCount, int reservedCount)
         {
             Title = title;
             ID = id;
+            Author = author;
             AvailableCount = availableCount;
             ReservedCount = reservedCount;
             bookCount = ReservedCount + AvailableCount;
+        }
+    }
+
+    public class Reservation
+    {
+        public string BookTitle { set; get; }
+        public int ClientID { set; get; }
+        public int BookID { set; get; }
+        public string ClientName { set; get; }
+        public DateTime ReservedDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public Reservation()
+        {
+            BookTitle = "None";
+            ClientID = -1;
+            BookID = -1;
+            ClientName = "None";
+            ReservedDate = DateTime.MinValue;
+            ReleaseDate = null;
+        }
+        public Reservation(int clientId , string clientName , int bookid, string bookTitle, DateTime date)
+        {
+            BookTitle = bookTitle;
+            ClientID = clientId;
+            ClientName = clientName;
+            ReservedDate = date;
+            BookID = bookid;
         }
     }
 }
