e957fb0ad03849a81b1a7fe641e09b4a53844c7f
smara <smara@LAPTOP-1E2M6GME>
2025-09-24 01:25:37 +0300

    modify function to deal with SQL

--- SLibrary.DataAccess/BookRepository.cs
+++ SLibrary.DataAccess/BookRepository.cs
@@ -6,104 +6,179 @@ using System.Threading.Tasks;
 using System.IO;
 using Shared;
 using System.IO.Ports;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SLibrary.DataAccess
 {
     public interface IBookRepository
     {
-        void Save();
-
-        void Load();
+        DataTable Load();
         void Add(Book b);
         Book GetByName(string title);
         int BookCount();
         List<Book> GetList();
+        void UpdateCounts(int bookId, int available, int reserved);
     }
     public class BookRepository : IBookRepository
     {
-        static string filePath = "BooksData.csv";
-        private List<Book> books = new List<Book>();
+ 
+        private string connectionString = "Server=.\\SQLEXPRESS;Database=SLibrary;Trusted_Connection=True";
         public BookRepository()
         {
-           Load();
-        }
-        public void Save()
-        {
-
-            List<string> lines = new List<string>();
-            lines.Add($"ID,Title,Author,Available,Reserved");
-
-            foreach (Book b in books)
-            {
-                lines.Add($"{b.ID},{b.Title},{b.Author},{b.AvailableCount},{b.ReservedCount}");
-            }
-            File.WriteAllLines(filePath, lines);
+          // Load();
         }
 
-        public void Load()
+        public DataTable Load()
         {
-            if (File.Exists(filePath))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines.Skip(1))
-                {
-                    var parts = line.Split(',');
-                    if (parts.Length < 5)
-                        continue;
-                    Book b = new Book(int.Parse(parts[0]), parts[1], parts[2], int.Parse(parts[3]), int.Parse(parts[4]));
-                    books.Add(b);
-                }
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Books", conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
             }
         }
-
         public void Add(Book b)
         {
-            bool found = false;
-            foreach (Book b2 in books)
+            using(SqlConnection conn = new SqlConnection(connectionString))
             {
-                if (b.Title == b2.Title)
+                conn.Open();
+
+                string check = "SELECT ID, Available FROM Books WHERE Title = @title";
+                using(SqlCommand checkcmd = new SqlCommand(check , conn))
                 {
-                    found = true;
-                    b2.AvailableCount++;
-                    b.ID = b2.ID;
-                    break;
+                    checkcmd.Parameters.AddWithValue("@title", b.Title);
+
+                    using (SqlDataReader reader = checkcmd.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+                            int id = (int)reader["ID"];
+                            int available = (int)reader["Available"];
+
+                            reader.Close();
+
+                            string update = "UPDATE Books SET Available = @available WHERE ID = @id";
+
+                            using (SqlCommand updatecmd = new SqlCommand(update, conn))
+                            {
+                                updatecmd.Parameters.AddWithValue("@available", available + 1);
+                                updatecmd.Parameters.AddWithValue("@id", id);
+                                updatecmd.ExecuteNonQuery();
+                            }
+                            return;
+                        }
+                    }
+                }
+
+                string insert = "INSERT INTO Books (Title, Author, Available, Reserved) VALUES (@title, @author, @available, @res)";
+                using (SqlCommand insertCmd = new SqlCommand(insert, conn))
+                {
+                    insertCmd.Parameters.AddWithValue("@title", b.Title);
+                    insertCmd.Parameters.AddWithValue("@author", b.Author);
+                    insertCmd.Parameters.AddWithValue("@available", b.AvailableCount);
+                    insertCmd.Parameters.AddWithValue("@res", b.ReservedCount);
+
+                    insertCmd.ExecuteNonQuery();
                 }
             }
-            if (found != true)
-            {
-                if (books.Count > 0)
-                {
-                    var maxId = books.Max(i => i.ID);
-                    b.ID = maxId + 1;
-                }
-                else
-                {
-                    b.ID = 1;
-                }
-                books.Add(b);
-            }
-            Save();
 
         }
 
         public Book GetByName(string title)
         {
-            foreach (Book b in books)
+            Book b = new Book();
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                if (b.Title == title)
-                    return b;
+                conn.Open();
+
+                string query = "SELECT ID, Title, Author, Available, Reserved FROM Books WHERE Title = @title";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@title", title);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Book book = new Book()
+                            {
+                                ID = (int)reader["ID"],
+                                Title = (string)reader["Title"],
+                                Author = (string)reader["Author"],
+                                AvailableCount = (int)reader["Available"],
+                                ReservedCount = (int)reader["Reserved"]
+                            };
+                            b = book;
+                        }
+                    }
+                }
             }
-            return null;
+
+            return b;
         }
 
         public int BookCount()
         {
-            return books.Count();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Books";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    return (int)cmd.ExecuteScalar();
+                }
+            }
         }
 
         public List<Book> GetList()
         {
+            List<Book> books = new List<Book>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT ID, Title, Author, Available, Reserved FROM Books";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Book book = new Book()
+                        {
+                            ID = (int)reader["ID"],
+                            Title = (string)reader["Title"],
+                            Author = (string)reader["Author"],
+                            AvailableCount = (int)reader["Available"],
+                            ReservedCount = (int)reader["Reserved"]
+                        };
+                        books.Add(book);
+                    }
+                }
+            }
+
             return books;
         }
+
+
+        public void UpdateCounts(int bookId, int available, int reserved)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Books SET Available=@avail, Reserved=@res WHERE ID=@id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@avail", available);
+                    cmd.Parameters.AddWithValue("@res", reserved);
+                    cmd.Parameters.AddWithValue("@id", bookId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
--- SLibrary.DataAccess/ReservationRepository.cs
+++ SLibrary.DataAccess/ReservationRepository.cs
@@ -5,76 +5,139 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SLibrary.DataAccess
 {
     public interface IReservationRepository
     {
-        void Save();
-
-        void Load();
+        DataTable Load();
         void Add(Reservation r);
         List<Reservation> GetReservation();
+        Reservation GetActiveReservation(string title, string clientName);
+        void Update(Reservation r);
     }
     public class ReservationRepository :IReservationRepository
     {
-        static string filePath = "ReservationsData.csv";
-        private List<Reservation> reservations = new List<Reservation>();
+
+        private string connectionString = "Server=.\\SQLEXPRESS;Database=SLibrary;Trusted_Connection=True";
 
         public ReservationRepository()
         {
-            Load();
-        }
-        public void Save()
-        {
-
-            List<string> lines = new List<string>();
-            lines.Add($"ID,Client_Name,Book_ID,Book_Title,Reserved_Date,Released_Date");
-
-            foreach (Reservation r in reservations)
-            {
-                string released = r.ReleaseDate.HasValue ? r.ReleaseDate.Value.ToString() : "-";
-                lines.Add($"{r.ClientID},{r.ClientName},{r.BookID},{r.BookTitle},{r.ReservedDate},{released}");
-            }
-            File.WriteAllLines(filePath, lines);
+           // Load();
         }
 
-        public void Load()
+        public DataTable Load()
         {
-            if (File.Exists(filePath))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines.Skip(1))
-                {
-                    var parts = line.Split(',');
-                    if (parts.Length < 5)
-                        continue;
-                    DateTime? releaseDate = parts[5] == "-" ? (DateTime?)null : DateTime.Parse(parts[5]);
-                    Reservation r = new Reservation(int.Parse(parts[0]), parts[1], int.Parse(parts[2]), parts[3], DateTime.Parse(parts[4]));
-                    r.ReleaseDate = releaseDate;
-                    reservations.Add(r);
-                }
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Reservations", conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
             }
         }
 
         public void Add(Reservation r)
         {
-            if(reservations.Count>0)
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                var maxID = reservations.Max(i => i.ClientID);
-                r.ClientID = maxID + 1;
+                conn.Open();
+
+                string insert = "INSERT INTO Reservations (Client_Name, Book_ID, Book_Title, ReservedDate, ReleaseDate) VALUES (@cname, @bid, @btitle, @rdate, @rel)";
+
+                using (SqlCommand cmd = new SqlCommand(insert, conn))
+                {
+                    cmd.Parameters.AddWithValue("@cname", r.ClientName);
+                    cmd.Parameters.AddWithValue("@bid", r.BookID);
+                    cmd.Parameters.AddWithValue("@btitle", r.BookTitle);
+                    cmd.Parameters.AddWithValue("@rdate", r.ReservedDate);
+                    cmd.Parameters.AddWithValue("@rel", r.ReleaseDate.HasValue ? (object)r.ReleaseDate.Value : DBNull.Value);
+
+
+                    cmd.ExecuteNonQuery();
+                }
             }
-            else
-            {
-                r.ClientID = 1;
-            }
-            reservations.Add(r);
-            Save();
         }
 
         public List<Reservation> GetReservation()
         {
+            List<Reservation> reservations = new List<Reservation>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Reservations";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Reservation res = new Reservation()
+                        {
+                            ClientID = (int)reader["ID"],
+                            ClientName = (string)reader["Client_Name"],
+                            BookID = (int)reader["Book_ID"],
+                            BookTitle = (string)reader["Book_Title"],
+                            ReservedDate = (DateTime)reader["ReservedDate"],
+                            ReleaseDate = reader["ReleaseDate"] == DBNull.Value ? (DateTime?)null : (DateTime)reader["ReleaseDate"]
+                        };
+                        reservations.Add(res);
+                    }
+                }
+            }
             return reservations;
         }
+
+        public Reservation GetActiveReservation(string title, string clientName)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"SELECT TOP 1 * FROM Reservations
+                         WHERE Book_Title=@title AND Client_Name=@client AND ReleaseDate IS NULL
+                         ORDER BY ReservedDate DESC";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@title", title);
+                    cmd.Parameters.AddWithValue("@client", clientName);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Reservation r = new Reservation()
+                            {
+                                ClientID = (int)reader["ID"],
+                                ClientName = (string)reader["Client_Name"],
+                                BookID = (int)reader["Book_ID"],
+                                BookTitle = (string)reader["Book_Title"],
+                                ReservedDate = (DateTime)reader["ReservedDate"],
+                                ReleaseDate = reader["ReleaseDate"] == DBNull.Value ? (DateTime?)null : (DateTime)reader["ReleaseDate"]
+                            };
+                            return r;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void Update(Reservation r)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Reservations SET ReleaseDate=@rel WHERE ID=@id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@rel", r.ReleaseDate.Value);
+                    cmd.Parameters.AddWithValue("@id", r.ClientID); 
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
