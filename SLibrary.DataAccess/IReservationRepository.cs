04bb194e52eb53011d9d2b2afb08440a67a43c0d
smara <smara@LAPTOP-1E2M6GME>
2025-09-24 14:44:38 +0300

    seperate interface for Reservation repo

---
+++ SLibrary.DataAccess/IReservationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SLibrary.DataAccess
+{
+    public interface IReservationRepository
+    {
+        List<Reservation> Load();
+        void Add(Reservation r);
+        List<Reservation> GetReservation();
+        Reservation GetActiveReservation(string title, string clientName);
+        void Update(Reservation r);
+    }
+}
