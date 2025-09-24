33bda433cc966b501f733ef79d4e43c8033a9bf1
smara <smara@LAPTOP-1E2M6GME>
2025-09-24 14:44:21 +0300

    seperate interface for Book repo

---
+++ SLibrary.DataAccess/IBookRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Shared;
+using System.IO.Ports;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace SLibrary.DataAccess
+{
+    public interface IBookRepository
+    {
+        List<Book> Load();
+        void Add(Book b);
+        Book GetByName(string title);
+        int BookCount();
+        List<Book> GetList();
+        void UpdateCounts(int bookId, int available, int reserved);
+    }
+   
+}
