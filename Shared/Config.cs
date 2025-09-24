1f3d673fdb9592a5c5012146f650f08d07247990
smara <smara@LAPTOP-1E2M6GME>
2025-09-24 14:51:22 +0300

    added Config class to loads appsettings.json

---
+++ Shared/Config.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public static class Config
+    {
+        public static IConfigurationRoot Configuration { get; }
+
+        static Config()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+    }
+}
