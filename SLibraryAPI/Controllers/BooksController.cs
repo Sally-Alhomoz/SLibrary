8cf694786051527e760c7ee766ca2c7968e5a0f6
smara <smara@LAPTOP-1E2M6GME>
2025-09-29 15:27:03 +0300

    Implement Web API controllers

---
+++ SLibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SLibrary.Business;
+
+namespace SLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookManager _bookManager;
+        public BooksController(BookManager bookMng)
+        {
+            _bookManager = bookMng;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = await _bookManager.GetBooks();
+            return Ok(books);
+        }
+
+        [HttpPost("Reserve")]
+        public async Task<IActionResult> ReserveBook(int bookid, string clientname)
+        {
+            var result = await _bookManager.Reserve(bookid, clientname);
+
+            if (result == false)
+            {
+                return BadRequest("Book Can Not be reserved");
+            }
+
+            return Ok("Book reserved successfully");
+        }
+
+        [HttpPost("Release")]
+        public async Task<IActionResult> ReleaseBook(int bookid, string clientname)
+        {
+            var result = await _bookManager.Release(bookid, clientname);
+
+            if (result == false)
+            {
+                return BadRequest("Book Can Not be released");
+            }
+
+            return Ok("Book released successfully");
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddBook(string name , string author)
+        {
+            await _bookManager.Addbook(name, author);
+            return Ok("Book added successfully");
+
+        }
+
+    }
+}
---
+++ SLibraryAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SLibrary.Business;
+
+namespace SLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly BookManager _bookManager;
+
+        public ReservationsController(BookManager bookmng)
+        {
+            _bookManager = bookmng;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetReservations()
+        {
+            var reservations = await _bookManager.GetReservations();
+            return Ok(reservations);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReservation(int id)
+        {
+            var reservation = await _bookManager.GetReservationById(id);
+            if (reservation == null) return NotFound();
+            return Ok(reservation);
+        }
+    }
+}
