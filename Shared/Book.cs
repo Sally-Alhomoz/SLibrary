22645745254bc4a0eb3ec58ac7cb15468172ebbf
smara <smara@LAPTOP-1E2M6GME>
2025-09-29 15:24:29 +0300

    Mark ID as Key

--- Shared/Book.cs
+++ Shared/Book.cs
@@ -3,64 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared
 {
     public class Book
     {
+        [Key]
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public int ID { get; set; }
-        public int bookCount { get; set; } = 0;
-        public int ReservedCount { get; set; }
-        public int AvailableCount { get; set; }
+        public int Reserved { get; set; }
+        public int Available { get; set; }
 
 
         public Book()
         {
             Title = "None";
-            ID = -1;
             Author = "None";
-            bookCount = 1;
-            AvailableCount = 1;
-            ReservedCount = 0;
+            Available = 1;
+            Reserved = 0;
         }
 
-        public Book(int id , string title, string author,int availableCount, int reservedCount)
+        public Book(string title, string author,int availableCount, int reservedCount)
         {
             Title = title;
-            ID = id;
             Author = author;
-            AvailableCount = availableCount;
-            ReservedCount = reservedCount;
-            bookCount = ReservedCount + AvailableCount;
-        }
-    }
-
-    public class Reservation
-    {
-        public string BookTitle { set; get; }
-        public int ClientID { set; get; }
-        public int BookID { set; get; }
-        public string ClientName { set; get; }
-        public DateTime ReservedDate { get; set; }
-        public DateTime? ReleaseDate { get; set; }
-        public Reservation()
-        {
-            BookTitle = "None";
-            ClientID = -1;
-            BookID = -1;
-            ClientName = "None";
-            ReservedDate = DateTime.MinValue;
-            ReleaseDate = null;
-        }
-        public Reservation(int clientId , string clientName , int bookid, string bookTitle, DateTime date)
-        {
-            BookTitle = bookTitle;
-            ClientID = clientId;
-            ClientName = clientName;
-            ReservedDate = date;
-            BookID = bookid;
+            Available = availableCount;
+            Reserved = reservedCount;
         }
     }
 }
