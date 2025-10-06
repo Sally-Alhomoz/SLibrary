e2d1272549b759efd5d6feddf37343f113644416
smara <smara@LAPTOP-1E2M6GME>
2025-10-06 15:24:00 +0300

    moved to the DAL

---
+++ SLibrary.DataAccess/SLibararyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SLibrary.DataAccess.Models;
+
+
+
+namespace SLibrary.DataAccess
+{
+    public class SLibararyDBContext : IdentityDbContext<AppUser>
+    {
+        public SLibararyDBContext(DbContextOptions<SLibararyDBContext> options) : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
