6fb9b381cff46af0cde1427b813c8bf30a4d9a60
smara <smara@LAPTOP-1E2M6GME>
2025-10-06 15:17:42 +0300

    added a Reservation DTO

---
+++ Shared/Reservationdto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shared
+{
+    public class Reservationdto
+    { 
+        public int ID { set; get; }
+        public string BookTitle { set; get; }
+        public int BookID { set; get; }
+        public string ClientName { set; get; }
+        public DateTime ReservedDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
