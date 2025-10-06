58b96335ea17f9ee56e1067bcd7942a58f994806
smara <smara@LAPTOP-1E2M6GME>
2025-10-06 15:25:48 +0300

    dto for new user

---
+++ Shared/dtoNewUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared
+{
+    public class dtoNewUser
+    {
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
