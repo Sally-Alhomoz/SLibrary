a4a377aa5de9b4161f6958cbfe7599f37f05a91b
smara <smara@LAPTOP-1E2M6GME>
2025-10-06 15:19:50 +0300

    folder contains the Repos Interfacec

---
+++ SLibrary.DataAccess/Interfacses/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SLibrary.DataAccess.Models;
+
+
+namespace SLibrary.DataAccess.Interfacses
+{
+    public interface IBookRepository
+    {
+        List<Book> Load();
+        void Add(Book b);
+        Book GetByName(string title);
+        int BookCount();
+        List<Book> GetBooks();
+        void UpdateCounts(int bookId, int available, int reserved);
+    }
+
+}
---
+++ SLibrary.DataAccess/Interfacses/IReservationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SLibrary.DataAccess.Models;
+
+namespace SLibrary.DataAccess.Interfacses
+{
+    public interface IReservationRepository
+    {
+        List<Reservation> Load();
+        void Add(Reservation r);
+        List<Reservation> GetReservations();
+        Reservation GetActiveReservation(string title, string clientName);
+        void Update(Reservation r);
+        Reservation GetReservationById(int id);
+    }
+}
