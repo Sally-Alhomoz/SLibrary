8e8bf7617c9cb5d52bd35ee468f4f1d022bba587
smara <smara@LAPTOP-1E2M6GME>
2025-10-06 15:32:11 +0300

    added a Account Controller

---
+++ SLibraryAPI/Controllers/AccountController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SLibrary.DataAccess.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Shared;
+
+namespace SLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration configuration;
+        public AccountController(UserManager<AppUser> userManager , IConfiguration config)
+        {
+            _userManager = userManager;
+            configuration = config;
+        }
+
+        /// <summary>
+        /// Register.
+        /// </summary>
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterNewUser(dtoNewUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.userName,
+                    Email = user.Email,
+                };
+                IdentityResult result = await _userManager.CreateAsync(appUser, user.password);
+
+                if (result.Succeeded)
+                {
+                    return Ok("Success");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+            }
+            return BadRequest(ModelState);
+
+        
+        }
+
+        /// <summary>
+        /// Login to Account.
+        /// </summary>
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LogIn(dtoLogin login)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByNameAsync(login.userName);
+
+                if(user != null)
+                {
+                    if (await _userManager.CheckPasswordAsync(user, login.password))
+                    {
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+                        var roles = await _userManager.GetRolesAsync(user);
+                        foreach(var role in roles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, role.ToString()));
+                        }
+                        //signingCredentials
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                        var token = new JwtSecurityToken(
+                            claims: claims,
+                            issuer: configuration["JWT:Issuer"],
+                            audience: configuration["JWT:Audience"],
+                            expires: DateTime.UtcNow.AddHours(1),
+                            signingCredentials: creds
+                            );
+                        var _token = new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo,
+                        };
+
+                        return Ok(_token);
+
+                    }
+                    else
+                    {
+                        return Unauthorized();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Name is invalid");
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+    }
+}
