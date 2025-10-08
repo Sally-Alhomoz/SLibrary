6bdafef2d0e5ac6d49f93b6339ebf43901b59ef5
smara <smara@LAPTOP-1E2M6GME>
2025-10-08 15:49:49 +0300

    added a User repo

---
+++ SLibrary.DataAccess/Interfacses/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SLibrary.DataAccess.Models;
+using System.Collections.Generic;
+
+namespace SLibrary.DataAccess.Interfacses
+{
+    public interface IUserRepository
+    {
+        List<User> GetUsers();
+        void Add(User user);
+        User GetByUsername(string name);
+    }
+}
---
+++ SLibrary.DataAccess/Repositories/DBUserRepository.cs
@@ -0,0 +1,42 @@
+﻿using SLibrary.DataAccess.Models;
+using SLibrary.DataAccess.Interfacses;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace SLibrary.DataAccess.Repositories
+{
+    public class DBUserRepository : IUserRepository
+    {
+        private readonly SLibararyDBContext _db;
+
+        public DBUserRepository(SLibararyDBContext db)
+        {
+            _db = db;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> users = _db.Users.ToList();
+            return users;
+        }
+
+        public void Add(User user)
+        {
+            var exist = _db.Users.FirstOrDefault(x => x.Id == user.Id);
+            if(exist != null)
+                throw new Exception("Username already exists.");
+            else
+            {
+                _db.Users.Add(user);
+            }
+            _db.SaveChanges();
+        }
+
+        public User GetByUsername(string name)
+        {
+            var user = _db.Users.FirstOrDefault(x => x.Username == name);
+            return user;
+        }
+    }
+}
