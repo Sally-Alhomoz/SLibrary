3d7044961b53e53158c1e8cdf16bd1901dcd9ff8
smara <smara@LAPTOP-1E2M6GME>
2025-10-08 15:51:20 +0300

    user entity

---
+++ SLibrary.DataAccess/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shared;
+using System;
+
+namespace SLibrary.DataAccess.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public Role Role { get; set; }
+        public string Checksum { get; set; }
+
+    }
+
+}
