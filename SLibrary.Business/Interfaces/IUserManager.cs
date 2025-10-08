9bba9cd549b0afbae9fe6a975d121a6190ee742e
smara <smara@LAPTOP-1E2M6GME>
2025-10-08 15:48:51 +0300

    added a User manager

---
+++ SLibrary.Business/Interfaces/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLibrary.Business.Interfaces
+{
+    public interface IUserManager
+    {
+        void Add(dtoNewUser u);
+        bool Validatelogin(dtoLogin user);
+        Userdto GetByUsername(string name);
+    }
+}
---
+++ SLibrary.Business/Managers/UserManager.cs
@@ -0,0 +1,59 @@
+﻿using Shared;
+using SLibrary.DataAccess.Interfacses;
+using SLibrary.DataAccess.Models;
+using SLibrary.Business.Interfaces;
+using System;
+
+namespace SLibrary.Business.Managers
+{
+    public class UserManager : IUserManager
+    {
+        IUserRepository userRepo;
+
+        public UserManager(IUserRepository repo)
+        {
+            userRepo = repo;
+        }
+
+        public void Add(dtoNewUser u)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = u.Username,
+                Email = u.Email,
+                Password = u.Password,
+                Role = Role.User,
+                Checksum = ""
+            };
+            userRepo.Add(user);
+        }
+
+        public bool Validatelogin(dtoLogin user)
+        {
+            var exist = userRepo.GetByUsername(user.Username);
+            if (exist != null && exist.Password == user.password)
+                return true;
+            else
+                return false;
+           
+        }
+
+        public Userdto GetByUsername(string name)
+        {
+            var exist = userRepo.GetByUsername(name);
+
+            if(exist != null)
+            {
+                var user = new Userdto
+                {
+                    Id = exist.Id,
+                    Username = exist.Username,
+                    Role = exist.Role
+                };
+                return user;
+            }
+            return null;
+        }
+    }
+}
