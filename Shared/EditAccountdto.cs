9553007ee36d40d129362b71a3f364d50fcbdd80
smara <smara@LAPTOP-1E2M6GME>
2025-10-30 10:04:28 +0300

    edit account DTO to edit user info

--- Shared/EditAccountdto.cs
+++ Shared/EditAccountdto.cs
@@ -4,27 +4,17 @@ namespace Shared
 {
     public class EditAccountdto
     {
+        //to display user info
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
         public bool IsActive { get; set; }
 
-        public class EditEmail
-        {
-            public string newEmail { get; set; }
-        }
+        //for edit email
+        public string newEmail { get; set; }
 
-        public class ResetPassword
-        {
-            public string OldPassword { get; set; }
-
-            public string NewPassword { get; set; }
-
-            public string ConfirmPassword { get; set; }
-        }
-
-        public EditEmail EditEmaildto { get; set; } = new EditEmail();
-        public ResetPassword ResetPassworddto { get; set; } = new ResetPassword();
+        //for edit username
+        public string newUsername { get; set; }
     }
 }
