e80adb5f41f899216c144b86d815d9c9e0c5b7d5
smara <smara@LAPTOP-1E2M6GME>
2025-11-04 13:36:43 +0300

    added client class to hold client info

---
+++ SLibrary.DataAccess/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SLibrary.DataAccess.Models
+{
+    public class Client
+    { 
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Address { get; set; }
+    }
+}
