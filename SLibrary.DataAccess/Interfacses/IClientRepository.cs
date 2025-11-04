51942ed7cfa22cc30cc5aefcbd19b9e212325d5e
smara <smara@LAPTOP-1E2M6GME>
2025-11-04 13:39:00 +0300

    added client repo

---
+++ SLibrary.DataAccess/Interfacses/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using SLibrary.DataAccess.Models;
+using System.Collections.Generic;
+
+namespace SLibrary.DataAccess.Interfacses
+{
+    public interface IClientRepository
+    {
+        List<Client> GetClients();
+        Client GetByPhoneNo(string no);
+        void Add(Client client);
+        Client GetByName(string name);
+    }
+}
