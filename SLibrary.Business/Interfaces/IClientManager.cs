abfacb1c309ac3d3fc1f7106e78c418e33d114ab
smara <smara@LAPTOP-1E2M6GME>
2025-11-04 13:38:33 +0300

    added client manager

---
+++ SLibrary.Business/Interfaces/IClientManager.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLibrary.Business.Interfaces
+{
+    public interface IClientManager
+    {
+        void Add(Clientdto dto);
+        Clientdto GetByPhoneNo(string no);
+        List<Clientdto> GetAllClients();
+        Clientdto GetByName(string name);
+        public IEnumerable<Clientdto> SearchClients(string term);
+    }
+}
---
+++ SLibrary.Business/Managers/ClientManager.cs
@@ -0,0 +1,108 @@
+﻿using Shared;
+using SLibrary.DataAccess.Interfacses;
+using SLibrary.DataAccess.Models;
+using SLibrary.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using SLibrary.DataAccess.SUnitOfWork;
+using System.Globalization;
+
+namespace SLibrary.Business.Managers
+{
+    public class ClientManager : IClientManager
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly ILogger<ClientManager> _logger;
+
+        public ClientManager(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public List<Clientdto> GetAllClients()
+        {
+            return _uow.DBClients.GetClients().Select(x => new Clientdto
+            {
+                Id = x.Id,
+                fullName=x.fullName,
+                PhoneNo= x.PhoneNo,
+                Address = x.Address
+            }).ToList();
+        }
+
+        public void Add(Clientdto dto)
+        {
+            var client = new Client
+            {
+                fullName=dto.fullName,
+                PhoneNo=dto.PhoneNo,
+                Address=dto.Address
+            };
+            _uow.DBClients.Add(client);
+            _uow.Complete();
+        }
+
+        public Clientdto GetByPhoneNo(string no)
+        {
+            var exist = _uow.DBClients.GetByPhoneNo(no);
+
+            if (exist != null)
+            {
+                var clinet = new Clientdto
+                {
+                    Id = exist.Id,
+                    fullName=exist.fullName,
+                    PhoneNo=exist.PhoneNo,
+                    Address=exist.Address
+                };
+                return clinet;
+            }
+            return null;
+        }
+
+        public Clientdto GetByName(string name)
+        {
+            var exist = _uow.DBClients.GetByName(name);
+
+            if (exist != null)
+            {
+                var clinet = new Clientdto
+                {
+                    Id = exist.Id,
+                    fullName = exist.fullName,
+                    PhoneNo = exist.PhoneNo,
+                    Address = exist.Address
+                };
+                return clinet;
+            }
+            return null;
+        }
+
+
+        public IEnumerable<Clientdto> SearchClients(string term)
+        {
+            string searchTerm = term?.ToLowerInvariant();
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                return Enumerable.Empty<Clientdto>();
+            }
+
+            var clients = _uow.DBClients.GetClients()
+                .Where(c => c.fullName.ToLower().Contains(searchTerm) ||
+                            c.PhoneNo.Contains(searchTerm))
+                .ToList();
+
+            var clientDtos = clients.Select(c => new Clientdto
+            {            
+                fullName = c.fullName,
+                PhoneNo = c.PhoneNo,
+                Address = c.Address
+            }).ToList(); 
+
+            return clientDtos;
+        }
+    }
+}
