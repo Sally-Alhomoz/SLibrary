4edf98445f35fd455b75864df74e5d563427791c
smara <smara@LAPTOP-1E2M6GME>
2025-11-10 11:24:22 +0300

    Reservation Services

---
+++ Blazor2/Services/IReservationService.cs
@@ -0,0 +1,10 @@
+﻿using Shared;
+
+namespace Blazor2.Services
+{
+    public interface IReservationService
+    {
+        Task<List<Reservationdto>> Read();
+        Task<(bool, string)> Release(string clientname, string title);
+    }
+}
---
+++ Blazor2/Services/ReservationService.cs
@@ -0,0 +1,39 @@
+﻿using Shared;
+using System.Net.Http.Json;
+
+namespace Blazor2.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly HttpClient _client;
+
+        public ReservationService(HttpClient http)
+        {
+            _client = http;
+        }
+
+        public async Task<List<Reservationdto>> Read()
+        {
+            var reservations = await _client.GetFromJsonAsync<List<Reservationdto>>("api/Reservations");
+            return reservations;
+        }
+
+        public async Task<(bool, string)> Release(string clientname, string title)
+        {
+            var endpoint = $"api/Reservations/Release?title={Uri.EscapeDataString(title)}&clientname={Uri.EscapeDataString(clientname)}";
+
+            var request = new HttpRequestMessage(HttpMethod.Delete, endpoint);
+            var response = await _client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return (true, $"Book '{title}' successfully released by {clientname}.");
+            }
+            else
+            {
+                string errormsg = await response.Content.ReadAsStringAsync();
+                return (false, $"Failed to release book: {response.StatusCode}. Details: {errormsg}");
+            }
+        }
+    }
+}
