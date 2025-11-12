f3809cb1cfea0104b19e64063ca1c06a3b9550b2
smara <smara@LAPTOP-1E2M6GME>
2025-11-12 15:31:54 +0300

    edit account view

---
+++ VueJS/src/Views/Account View/EditAccount.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container py-5">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6">
+        <h2 class="text-center mb-5 text-dark fw-bold">Edit Account</h2>
+
+        <div class="card shadow-lg border-0 rounded-xl">
+          <div class="card-body p-5">
+            <form @submit.prevent="editEmail">
+              <div class="mb-3">
+                <input v-model="newEmail"
+                       placeholder="New email"
+                       type="email"
+                       required
+                       class="form-control" />
+              </div>
+
+              <button type="submit" class="btn btn-primary w-100">Update</button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import axios from 'axios'
+
+  const newEmail = ref('')
+
+  const router = useRouter()
+
+  const API_BASE_URL = 'https://localhost:7037';
+
+  const getToken = () => {
+    return localStorage.getItem('token');
+  };
+
+  const editEmail = async () => {
+    const token = getToken();
+    if (!token) {
+      error.value = 'You must be logged in to view client information.'
+      return
+    }
+
+    const params = {
+      NewEmail: newEmail.value
+    };
+
+    await axios.patch(`${API_BASE_URL}/api/Account/EditEmail`, params, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    router.push('/app/profile')
+
+  }
+
+</script>
