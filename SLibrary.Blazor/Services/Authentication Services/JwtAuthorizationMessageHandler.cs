00d42a5439b925bb8babe1c1b07ab1fb5975d250
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 12:23:27 +0300

    Add JwtAuthorizationMessageHandler for automatic Bearer token adding

---
+++ SLibrary.Blazor/Services/Authentication Services/JwtAuthorizationMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace SLibrary.Blazor.Services.Authentication_Services
+{
+
+    public class JwtAuthorizationMessageHandler : DelegatingHandler
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public JwtAuthorizationMessageHandler(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+            InnerHandler = new HttpClientHandler();
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _localStorage.GetItemAsync<string>("jwt");
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
+
+
