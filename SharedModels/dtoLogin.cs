3024df02860d0ad57ba0492ec8f24216514e80c4
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 11:48:13 +0300

    DTO class for login

---
+++ SharedModels/dtoLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared
+{
+    public class dtoLogin
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string password { get; set; }
+
+    }
+}
