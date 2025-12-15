3370149ae744bde303266ad08885ab237a9e5d4e
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 11:48:50 +0300

    DTO class for reservation

---
+++ SharedModels/Reservationdto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Shared
+{
+    public class Reservationdto
+    { 
+        public int ID { set; get; }
+        public string BookTitle { set; get; }
+        public int BookID { set; get; }
+        public string ClientName { set; get; }
+        public string ReservedBy { set; get; }
+        public DateTime ReservedDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
+        public TimeSpan ReservationPeriod
+        {
+            get
+            {
+                if (ReleaseDate.HasValue)
+                {
+                    return ReleaseDate.Value.Subtract(ReservedDate);
+                }
+                return TimeSpan.Zero;
+            }
+        }
+
+    }
+}
