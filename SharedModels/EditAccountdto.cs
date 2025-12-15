215e35f7e40b1c9cc82e3d95abcbdf2c772cd4f1
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 11:50:40 +0300

    DTO class to edit account info

---
+++ SharedModels/EditAccountdto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shared
+{
+    public class EditAccountdto
+    {
+        //to display user info
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class EditEmaildto
+    {
+        public string newEmail { get; set; }
+    }
+}
