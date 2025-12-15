faf6320442a69262e81cc1c2cfb4122271b44ccc
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 11:50:14 +0300

    a user DTO class

---
+++ SharedModels/Userdto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shared
+{
+    public class Userdto
+    {
+        public Guid Id { get; set; }     
+        public string Username { get; set; } 
+        public Role Role { get; set; }
+        public bool IsActive { get; set; }
+        public string Email { get; set; }
+    }
+}
