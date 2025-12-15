10ef17321bb8f2d1c7535b88269515a41f8726d7
smara <smara@LAPTOP-1E2M6GME>
2025-12-15 11:47:18 +0300

    Client DTO class

---
+++ SharedModels/Clientdto.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Shared
+{
+    public class Clientdto
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Address { get; set; }
+    }
+}
